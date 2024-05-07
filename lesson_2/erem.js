9bb51faad75d37a2612e325c7421f4a1c6211d2e
erbolbaktiaruulu <baktiaruuluerbol5@gmail.com>
2024-05-07 13:09:41 +0900

    commit -m 'test'

--- lesson_2/erem.js
+++ lesson_2/erem.js
@@ -156,3 +156,149 @@
 // console.log(res);
 
 
+
+
+
+// Задание №2
+// Используя цикл for вывести все четные числа от 1 до 100;
+// for(let i=0; i<=100;i+=2){
+//     console.log(i);
+// }
+
+
+
+// Подсчитайте сумму чисел в диапазоне от 1 до 20 с помощью цикла for, while и do...while.
+// let num = 0;
+// for(let i =1;i<=20;i++){
+//     num+=i
+// }
+// console.log(num);
+
+
+
+
+// Задание №5
+// Напишите функцию, которая принимает 2 числа и возвращает 1, если первое число больше, чем второе; -1, если первое число меньше, чем второе, и 0, если числа равны.
+// function func(a,b){
+//     if(a>b){
+//         return -1
+//     }else if(a<b){
+//         return 0
+//     }else if(a===b){
+//          return '='
+//     }
+// }
+// console.log(func(3,4));
+
+
+
+
+
+// Задание №5
+// Напишите функцию, где пользователь вводит любое целое положительное число. А программа суммирует все числа от 1 до введенного пользователем числа.
+
+// function func(num){
+//     let numm = 0
+//     for(let i =1; i<=num;i++){
+//         numm+=i
+//     }
+//     return numm
+// }
+// console.log(func(5));
+
+
+
+
+
+// const str = "I am a Megalodon, oceans feeling like a pond"
+// let num = str.split(' ')
+// let ser = num.reduce((acc,el)=>acc.length>el.length? acc: el)
+// console.log(ser);
+
+
+
+
+
+// Задание №7
+// Напишите функцию, которая принимает параметром элемент и возвращает его тип (typeof)
+// function func(el){
+//     if(typeof el){
+//         return typeof el
+//     }
+// }
+// console.log(func(5));
+
+
+
+
+
+
+// Задание №8
+// Напишите функцию для расчета площади и окружности круга по радиусу.
+// Ввести радиус:
+// Вывести площадь и окружность
+// function func(s){
+//     let num = 3.14*(s**2)
+//     return `${num} радиус`
+// }
+// console.log(func(5));
+
+
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// function func(arr){
+//     let num = []
+//   return arr.map(el=>{
+//     if(el%2===0){
+//       num.push(el)
+//     }
+//     return num
+
+//   })
+// }
+// console.log(func(arr));
+
+
+
+
+
+function func(arr){
+   return arr.filter(el=>el<0)
+}
+console.log(func([7, -5, -29, 0, 62, 4, 5, -6, -27]));
+
+
+
+function rgb(n,u,m){
+    let ter =  n.toString()
+    let ter1 = u.toString()
+    let ter2= m.toString()
+    if(ter === '' && ter1===0&&ter2===0){
+        return 0
+    }else{
+        return `rgb(${ter} ${ter1} ${ter2})`  
+    }
+    
+}
+console.log(rgb(2,3,4));
+
+
+function func(a,b){
+    if(a.length === b.length&&a===b){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(func(234,234));
+
+
+
+function func1(a,b){
+    return a.filter(el=>{
+        if(el%b==0){
+            return el
+        } 
+    })
+}
+console.log(func1([1, 2, 3, 4, 5, 6],2));
